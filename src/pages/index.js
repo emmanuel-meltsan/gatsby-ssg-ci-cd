8bdd051d2dbc61a0416dfdebc1b4a65815e8f111
Emmanuel Lopez <emmanuel.lopez@meltsan.com>
2025-10-06 17:00:55 -0600

    testing gh action

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,11 +37,16 @@ const BlogIndex = ({ data, location }) => {
                 itemType="http://schema.org/Article"
               >
                 <header>
+                  <H1>
+                    PRACTICE DEPLYMENT
+                  </H1>
+
                   <h2>
                     <Link to={post.fields.slug} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
+
                   <small>{post.frontmatter.date}</small>
                 </header>
                 <section>
