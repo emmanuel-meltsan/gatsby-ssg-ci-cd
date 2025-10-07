ea7139cb4b889a6b5c8713421f201f533e18a35b
Emmanuel Lopez <emmanuel.lopez@meltsan.com>
2025-10-06 18:08:07 -0600

    update: author name and location

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,8 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Emmanuel`,
+      summary: `who lives and works in Mexico building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
